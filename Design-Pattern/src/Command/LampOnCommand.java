c7a9e244ad0b3398454c201ef4fbca0e86696d9e
codingzzang123 <hosun9752@gmail.com>
2022-06-15 20:25:32 +0900

    Command Pattern

---
+++ Design-Pattern/src/Command/LampOnCommand.java
@@ -0,0 +1,13 @@
+package Command;
+
+public class LampOnCommand implements Command{
+    private Lamp lamp;
+
+    public LampOnCommand(Lamp lamp){
+        this.lamp = lamp;
+    }
+
+    public void run(){
+        lamp.turnOn();
+    }
+}
